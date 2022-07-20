be480a8ecd198ebecca62dfa683f689547cd8170
Ilias Antonopoulos <ilias.antonopoulos@yahoo.gr>
2022-07-20 14:40:37 +0300

    [test]: introduce a first test case in order to have green CIs

---
+++ tests/callbacks/test_slack.py
@@ -0,0 +1,52 @@
+from unittest import mock
+
+import pytest
+import responses
+import tensorflow as tf
+
+
+@pytest.fixture
+def callback():
+    with mock.patch("tfnotify.SlackCallback") as callback_:
+        yield callback_(webhook_url="https://url.to/webhook")
+
+
+class TestSlackCallback:
+    @responses.activate
+    def test_callback_occurs_on_train_end(self, callback):
+
+        responses.add(
+            responses.POST,
+            url="https://url.to/webhook",
+            status=200,
+        )
+
+        # define tf.keras model to add callback to
+        model = tf.keras.Sequential(name='neural-network')
+        model.add(tf.keras.layers.Dense(1, input_dim=784))
+        model.compile(
+            optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.1),
+            loss="mean_squared_error",
+            metrics=["mean_absolute_error"],
+        )
+
+        # load example MNIST data and pre-process it
+        (x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+        x_train = x_train.reshape(-1, 784).astype("float32") / 255.0
+
+        # limit the data to 1000 samples
+        x_train = x_train[:1000]
+        y_train = y_train[:1000]
+
+        # initiate training
+        model.fit(
+            x_train,
+            y_train,
+            batch_size=128,
+            epochs=2,
+            verbose=0,
+            validation_split=0.5,
+            callbacks=[callback],
+        )
+
+        assert callback.on_train_end.call_count == 1
