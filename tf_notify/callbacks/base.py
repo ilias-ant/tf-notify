78d39674b064890448356f44a39a2a52a14633dd
Ilias Antonopoulos <ilias.antonopoulos@yahoo.gr>
2022-07-24 14:57:34 +0300

    [style]: BaseNotificationCallback - add NotImplementedError message

--- tf_notify/callbacks/base.py
+++ tf_notify/callbacks/base.py
@@ -3,4 +3,6 @@ from tensorflow import keras
 
 class BaseNotificationCallback(keras.callbacks.Callback):
     def on_train_end(self, logs=None):
-        raise NotImplementedError
+        raise NotImplementedError(
+            "BaseNotificationCallback should not be used directly - instead use callbacks that inherit from this callback."
+        )
